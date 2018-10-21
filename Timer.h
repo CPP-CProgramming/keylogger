054a2025083f8c257494d7642b411ec16050e9ba
neogeoss <neogeo-s@hanmail.net>
2018-10-21 19:29:49 +0900

    Timer thread handling logic

--- Timer.h
+++ Timer.h
@@ -21,6 +21,91 @@ class Timer{
             Function()();
     }
 
+    void ThreadFunc()
+    {
+        if(CallNumber == Infinite)
+            while(Alive)
+                SleepAndRun();
+        else
+            while(repeat_count--)
+                SleepAndRun();
+    }
+
+    public:
+        static const long Infinite = -1L;
+
+        Timeer()
+        {
+
+        }
+
+        Timer(const std::function<void(void)> &f) : funct (f)
+        {
+
+        }
+
+        Timer(const std::function<void(void)> &f,
+              const unsigned long &i,
+              const long repeat = Timer::Infinite) : funct (f),
+                                                     interval(std::chrono::milliseconds(i)),
+                                                     CallNumber(repeat){}
+
+        void Start(bool Async = true)
+        {
+            if(IsAlive())
+                return;
+            Alive = true;
+            repeat_count = CallNumber;
+            if(Async)
+                Thread = std::thread(ThreadFunc, this);
+            else
+                this->ThreadFunc();
+        }
+
+        void Stop()
+        {
+            Alive = false;
+            Thread.join();
+        }
+
+        void setFunction(const std::function<void(void)> &f)
+        {
+            funct = f;
+        }
+
+        bool IsAlive() const {return Alive;}
+
+        void RepeatCount(const long r)
+        {
+            if(Alive)
+                return;
+            CallNumber = r;
+        }
+
+        long GetLeftCount() const {return repeat_count;}
+
+        long RepeatCount () const {return CallNumber;}
+
+        void SetInterval(const unsigned long &i)
+        {
+            if(Alive)
+                return;
+
+            interval = std::chrono::milliseconds(i);
+
+
+        }
+
+        unsigned long Interval() const
+        {
+            return interval.count();
+        }
+
+        const std::function<void(void)> &Function() const
+        {
+            return funct;
+        }
+
 
 };
 
